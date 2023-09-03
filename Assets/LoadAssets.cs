dfcbdd6dd9a282c4e31e54fa3b809663a8746e08
Junchi Liu <Junchi.Liu@student.uts.edu.au>
2023-09-03 18:37:30 +1000

    70 percent complete

--- Assets/LoadAssets.cs
+++ Assets/LoadAssets.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
+    [SerializeField]
+    private GameObject blueObj;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = 60;
+        GameObject redInstance = Instantiate(redObj);
+        GameObject blueInstance = Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
