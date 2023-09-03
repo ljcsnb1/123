32c32a428b049477abb80dab06d9bdcdf0bef32a
Junchi Liu <Junchi.Liu@student.uts.edu.au>
2023-09-03 18:54:43 +1000

    90 percent complete

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
+    public Renderer rend;
     private int i = 3; // Initialize 'i' to 3
     // Start is called before the first frame update
     void Start()
@@ -16,5 +17,9 @@ public class ConsolePrint : MonoBehaviour
     {
         Debug.Log(gameObject.name + ":" + i);
         i++;
+        if (i == 100)
+        {
+            rend.enabled = false; // Hide the renderer when 'i' reaches 100
+        }
     }
 }
